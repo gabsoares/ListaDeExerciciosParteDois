d645607ef17c726868eec736c8eef785b98db8e4
gabsoares <soaresgabriel68@gmail.com>
2023-05-16 23:29:34 -0300

    Atualização do exercício

--- ListaDeExerciciosParteDois/Program.cs
+++ ListaDeExerciciosParteDois/Program.cs
@@ -1,15 +1,14 @@
-﻿int option = 1;
+﻿int number = 1;
 
-while (option != 0)
+while (number != 0)
 {
     Console.Write("Digite um número para verificar se o mesmo eh par ou impar: ");
-    int number = Convert.ToInt32(Console.ReadLine());
+    number = Convert.ToInt32(Console.ReadLine());
 
-    if (number % 2 == 0)
+    if (number % 2 == 0 && number != 0)
         Console.WriteLine($"O número digitado {number} eh par!");
+    else if (number == 0)
+        Console.WriteLine("A aplicação encerrará...");
     else
-        Console.WriteLine($"O número digitado {number} eh impar!");
-
-    Console.Write("Digite 0 caso queira sair e 1 para continuar verificando: ");
-    option = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"O número digitado {number} eh impar");
 }
